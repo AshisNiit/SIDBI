cf08d5ac00ca575b1c6442d2022f14ca644b71cd
ashis9437810184@gmail.com <ashis9437810184@gmail.com>
2024-04-21 14:02:25 +0530

    SIDBI SWF

---
+++ swf/src/main/java/com/sidbi/swf/SwfApplication.java
@@ -0,0 +1,13 @@
+package com.sidbi.swf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwfApplication.class, args);
+	}
+
+}
